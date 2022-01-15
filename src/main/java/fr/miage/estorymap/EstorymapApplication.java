d3a54af6628b193ee60abe984fdbfb2977bd2c7a
aymeric.henouille <aymeric.henouille.etu@univ-lille.fr>
2022-01-15 10:00:36 +0100

    init project

---
+++ src/main/java/fr/miage/estorymap/EstorymapApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.estorymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstorymapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstorymapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/estorymap/EstorymapApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.estorymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstorymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
