54bed80eedfcceb8b6ba6e678285f87a2ca23809
defossez <aymeric.defossez.etu@univ-lille.fr>
2022-03-03 18:58:55 +0100

    jpp

--- src/main/java/fr/miage/estorymap/repository/UserRepository.java
+++ src/main/java/fr/miage/estorymap/repository/UserRepository.java
@@ -2,6 +2,7 @@ package fr.miage.estorymap.repository;
 
 import fr.miage.estorymap.entity.User;
 import fr.miage.estorymap.entity.Workspace;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +10,8 @@ import org.springframework.data.repository.CrudRepository;
 @EnableJpaRepositories
 public interface UserRepository extends CrudRepository<User, String> {
 
-    @Query(value = "SELECT w FROM workspace w WHERE w.idw IN (" +
-            "SELECT workspace_id FROM workspaces WHERE user_id = ?1)", nativeQuery = true)
-    Iterable<Workspace> findAllSharedWorkspaceByUserId(String id);
+    @Query(value = "SELECT w FROM workspace w WHERE w.idw = ?1", nativeQuery = true)
+    Iterable<Workspace> findAllSharedWorkspaceByUserId(Long id);
+
+    @NamedQuery(name = "findAllSharedWorkspaceByUserId", query = "SELECT w FROM workspace w WHERE w.idw = 1")
 }
